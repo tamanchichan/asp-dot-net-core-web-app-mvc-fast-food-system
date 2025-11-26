6dcd290f0d0624c7e506413163eecc4741b16ed6
tamanchichan <tamanchichan@gmail.com>
2025-11-26 17:27:40 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace asp_dot_net_core_web_app_mvc_fast_food_system.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
