ba4e516d33603e820dc2c66a4754687762ba9d29
tamanchichan <tamanchichan@gmail.com>
2025-11-26 17:32:16 -0600

    Add Identity packages

---
+++ Areas/Identity/Data/FastFoodSystemDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace asp_dot_net_core_web_app_mvc_fast_food_system.Areas.Identity.Data;
+
+public class FastFoodSystemDbContext : IdentityDbContext<IdentityUser>
+{
+    public FastFoodSystemDbContext(DbContextOptions<FastFoodSystemDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using asp_dot_net_core_web_app_mvc_fast_food_system.Areas.Identity.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("FastFoodSystemDbContextConnection") ?? throw new InvalidOperationException("Connection string 'FastFoodSystemDbContextConnection' not found.");;
+
+builder.Services.AddDbContext<FastFoodSystemDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<FastFoodSystemDbContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
