019e4071baca8e9a4da3484bbf4a7203dede3656
tamanchichan <tamanchichan@gmail.com>
2025-11-27 03:14:37 -0600

    Update constructor, call '()' constructor explicitly

---
+++ Areas/Identity/Data/DefaultUsers.cs
@@ -0,0 +1,11 @@
+﻿namespace asp_dot_net_core_web_app_mvc_fast_food_system.Areas.Identity.Data
+{
+    public class DefaultUsers
+    {
+        public Dictionary<string, string> Users = new Dictionary<string, string>() // username, password
+        {
+            {"admin", "admin" },
+            {"staff", "staff" }
+        };
+    }
+}
