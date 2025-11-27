8972da89cc2f2d4ddfb16e295cf3aa27ca935bc0
tamanchichan <tamanchichan@gmail.com>
2025-11-27 04:43:52 -0600

    Configure Identity password rules and enable roles

--- Program.cs
+++ Program.cs
@@ -4,16 +4,42 @@ using asp_dot_net_core_web_app_mvc_fast_food_system.Areas.Identity.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration.GetConnectionString("FastFoodSystemDbContextConnection") ?? throw new InvalidOperationException("Connection string 'FastFoodSystemDbContextConnection' not found.");;
+var connectionString = builder.Configuration.GetConnectionString("FastFoodSystemDbContextConnection") ?? throw new InvalidOperationException("Connection string 'FastFoodSystemDbContextConnection' not found."); ;
 
 builder.Services.AddDbContext<FastFoodSystemDbContext>(options => options.UseSqlServer(connectionString));
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<FastFoodSystemDbContext>();
+builder.Services.AddDefaultIdentity<IdentityUser>
+    (options =>
+    {
+        options.SignIn.RequireConfirmedAccount = true;
+        options.Password.RequireDigit = false;
+        options.Password.RequireLowercase = false;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequiredLength = 3;
+    })
+    .AddRoles<IdentityRole>() // Enable Roles
+    .AddEntityFrameworkStores<FastFoodSystemDbContext>(); // Use the FastFoodSystemDbContext for Identity
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+using (IServiceScope scope = app.Services.CreateScope())
+{
+    IServiceProvider services = scope.ServiceProvider;
+
+    try
+    {
+        await DefaultUsers.Initialize(services);
+    }
+    catch (Exception ex)
+    {
+        // Log errors or handle them as needed
+        Console.WriteLine(ex.Message);
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
