268c3bab77068f794852fd39e80841a2ecdaa81c
tamanchichan <tamanchichan@gmail.com>
2025-12-02 22:23:16 -0600

    Create AddProductToCart action in CartController

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -1,5 +1,8 @@
 ﻿using asp_dot_net_core_web_app_mvc_fast_food_system.Areas.Identity.Data;
+using asp_dot_net_core_web_app_mvc_fast_food_system.Enums;
 using asp_dot_net_core_web_app_mvc_fast_food_system.Models.Base;
+using asp_dot_net_core_web_app_mvc_fast_food_system.Models.CartProducts;
+using asp_dot_net_core_web_app_mvc_fast_food_system.Models.Products;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -29,6 +32,7 @@ namespace asp_dot_net_core_web_app_mvc_fast_food_system.Controllers
         {
             Cart cart = _context.Carts?
                 .Include(c => c.CartProducts)
+                .ThenInclude(cp => cp.Product)
                 .FirstOrDefault(c => c.UserId == _userManager.GetUserId(User));
 
             if (cart == null)
@@ -44,6 +48,66 @@ namespace asp_dot_net_core_web_app_mvc_fast_food_system.Controllers
             }
 
             return View(cart);
+        }
+
+        public IActionResult AddProductToCart
+        (
+            Guid id,
+            string? instructions = null,
+            BeverageOption? beverageOption = null,
+            FoodOption? foodOption = null,
+            FoodSize? foodSize = null,
+            SauceOption? sauceOption = null
+        )
+        {
+            Cart cart = _context.Carts?
+                .Include(c => c.CartProducts)
+                .ThenInclude(cp => cp.Product)
+                .FirstOrDefault(c => c.UserId == _userManager.GetUserId(User));
+
+            if (cart == null)
+            {
+                ModelState.AddModelError(string.Empty, "Cart not found for the current user. Contact support.");
+                return RedirectToAction("Index");
+            }
+
+            Product product = _context.Products.FirstOrDefault(p => p.Id == id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            CartProduct cartProduct = cart.CartProducts.FirstOrDefault(cp => cp.ProductId == product.Id);
+
+            if (cartProduct == null)
+            {
+                if (product is FoodProduct)
+                {
+                    cartProduct = new CartFoodProduct
+                    {
+                        CartId = cart.Id,
+                        Cart = cart,
+                        ProductId = product.Id,
+                        Product = product,
+                        Instructions = instructions,
+                        FoodOption = foodOption,
+                        FoodSize = foodSize,
+                        Quantity = 1
+                    };
+                }
+                // add the rest of the derived class
+
+                cart.CartProducts.Add(cartProduct);
+            }
+            else
+            {
+                cartProduct.Quantity++;
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Cart");
         }
     }
 }
