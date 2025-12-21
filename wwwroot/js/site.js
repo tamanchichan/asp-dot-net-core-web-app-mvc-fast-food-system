36d5c64eae79ebac83ff38b34e0cb0f2fa02c682
tamanchichan <tamanchichan@gmail.com>
2025-12-21 12:50:56 -0600

    Update js function to save scroll position only at html elements that contains 'save-scroll-position' class

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -14,5 +14,19 @@
     });
 }
 
-// Call the function to activate it
-keepScrollPosition();
+// Call the function to activate it to work throughout the site
+// keepScrollPosition();
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll(".save-scroll-position").forEach((link) => {
+        link.addEventListener("click", () => {
+            sessionStorage.setItem("scrollPosition", window.scrollY);
+        });
+    });
+
+    const scrollY = sessionStorage.getItem("scrollPosition");
+    if (scrollY !== null) {
+        window.scrollTo(0, parseInt(scrollY, 10));
+        sessionStorage.removeItem("scrollPosition");
+    }
+});
