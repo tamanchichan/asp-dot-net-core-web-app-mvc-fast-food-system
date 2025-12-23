8cfb16958e8ca16201e589f28d50a470d27d5fa7
tamanchichan <tamanchichan@gmail.com>
2025-12-23 06:51:22 -0600

    Add helper to get sauce product abbreviation

---
+++ Helpers/SauceOptionExtensions.cs
@@ -0,0 +1,39 @@
+﻿using asp_dot_net_core_web_app_mvc_fast_food_system.Enums;
+
+namespace asp_dot_net_core_web_app_mvc_fast_food_system.Helpers
+{
+    public class SauceOptionExtensions
+    {
+        public static string GetSauceAbbreviation(SauceOption? sauce)
+        {
+            if (sauce == null)
+            {
+                return "";
+            }
+
+            switch (sauce)
+            {
+                case SauceOption.SoySauce:
+                    return "S.S.";
+                case SauceOption.PlumSauce:
+                    return "P.S.";
+                case SauceOption.HotSauce:
+                    return "H.S.";
+                case SauceOption.SweetAndSourSauce:
+                    return "S.S.S.";
+                case SauceOption.HoneyLemonSauce:
+                    return "H.L.S.";
+                case SauceOption.HoneyGarlicSauce:
+                    return "H.G.S.";
+                case SauceOption.HotHoneyGarlicSauce:
+                    return "H.H.G.S.";
+                case SauceOption.BlackBeanGarlicSauce:
+                    return "B.B.G.S.";
+                case SauceOption.CurrySauce:
+                    return "C.S.";
+                default:
+                    return "";
+            }
+        }
+    }
+}
