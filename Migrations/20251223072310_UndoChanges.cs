3030ea9d32b697e0d4ad85a8ae17d6d2bdbf885e
tamanchichan <tamanchichan@gmail.com>
2025-12-23 01:28:01 -0600

    Undo migration from trying to set "price"

---
+++ Migrations/20251223072310_UndoChanges.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace asp_dot_net_core_web_app_mvc_fast_food_system.Migrations
+{
+    /// <inheritdoc />
+    public partial class UndoChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "CartProducts");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "CartProducts",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
