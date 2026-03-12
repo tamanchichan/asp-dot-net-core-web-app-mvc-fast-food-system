c2c9578d6d86f13be3f4cf4a17b32e213a58820d
tamanchichan <tamanchichan@gmail.com>
2026-03-12 12:53:33 -0500

    Add Customers' Controller and View; Create Customers' Index View

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,32 @@
+﻿using asp_dot_net_core_web_app_mvc_fast_food_system.Areas.Identity.Data;
+using asp_dot_net_core_web_app_mvc_fast_food_system.Models.Base;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace asp_dot_net_core_web_app_mvc_fast_food_system.Controllers
+{
+    [Authorize]
+    public class CustomersController : Controller
+    {
+        private readonly ILogger<CustomersController> _logger;
+
+        private readonly UserManager<SystemUser> _userManager;
+
+        private readonly FastFoodSystemDbContext _context;
+
+        public CustomersController(ILogger<CustomersController> logger, UserManager<SystemUser> userManager, FastFoodSystemDbContext context)
+        {
+            _logger = logger;
+            _userManager = userManager;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            HashSet<Customer> customers = _context.Customers.ToHashSet();
+
+            return View(customers);
+        }
+    }
+}
