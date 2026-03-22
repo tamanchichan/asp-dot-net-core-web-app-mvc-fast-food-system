32e76436bd0dfe398c3dfea02fb8a58864b04b70
tamanchichan <tamanchichan@gmail.com>
2026-03-21 21:14:00 -0500

    Create incrementProductQuantity and decrementProductQuantity functions to handle POST requests

---
+++ wwwroot/js/Views/Shared/_ProductItemsFields.js
@@ -0,0 +1,29 @@
+﻿export function incrementProductQuantity(productId) {
+    fetch(`/Cart/IncrementProduct?id=${productId}`, {
+        method: "POST"
+    })
+        .then(response => response.json())
+        .then(data => {
+            const row = document.querySelector(`.product-item[data-id="${productId}"]`);
+
+            row.querySelector(".product-quantity").textContent = data.quantity;
+        })
+};
+
+export function decrementProductQuantity(productId) {
+    fetch(`/Cart/DecrementProduct?id=${productId}`, {
+        method: "POST"
+    })
+        .then(response => response.json())
+        .then(data => {
+            const element = document.querySelector(`.product-item[data-id="${productId}"]`);
+
+            element.querySelector(".product-quantity").textContent = data.quantity;
+        })
+};
+
+export function test() {
+    console.log("hello, world");
+}
+
+console.log('hello');
