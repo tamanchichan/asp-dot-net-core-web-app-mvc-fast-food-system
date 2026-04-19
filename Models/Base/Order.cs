14e3492a45492f441b437d5f4fb27825af33833d
tamanchichan <tamanchichan@gmail.com>
2026-04-18 21:51:39 -0500

    Remove ProductsTotalPrice

--- Models/Base/Order.cs
+++ Models/Base/Order.cs
@@ -43,7 +43,7 @@ namespace asp_dot_net_core_web_app_mvc_fast_food_system.Models.Base
 
         public DateTime ReadyTime { get; set; }
 
-        public decimal ProductsTotalPrice
+        public decimal? AdditionalCharge { get; set; } = 0m;
         {
             get
             {
