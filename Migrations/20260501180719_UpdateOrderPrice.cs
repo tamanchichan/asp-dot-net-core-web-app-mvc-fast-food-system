4255b6c7f9bb5c8dcb64783a8d23e3c29cd903f8
tamanchichan <tamanchichan@gmail.com>
2026-05-01 13:14:38 -0500

    Update database

---
+++ Migrations/20260501180719_UpdateOrderPrice.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace asp_dot_net_core_web_app_mvc_fast_food_system.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateOrderPrice : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "OrderProducts",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "OrderProducts");
+        }
+    }
+}
