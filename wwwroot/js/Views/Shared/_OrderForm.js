cd34af630c9ce4324c9608e71bc902354c4419b7
tamanchichan <tamanchichan@gmail.com>
2026-05-01 13:15:49 -0500

    Move some properties from _OrderForm.js to orderFormDetails.js, everything related to customer

--- wwwroot/js/Views/Shared/_OrderForm.js
+++ wwwroot/js/Views/Shared/_OrderForm.js
@@ -1,64 +1,4 @@
-﻿const observationsInput = document.getElementById("observations");
-observationsInput.addEventListener("input", () => {
-    localStorage.setItem("observations", observationsInput.value);
-});
-observationsInput.value = localStorage.getItem("observations");
-
-const customerNameInput = document.getElementById("customerName");
-customerNameInput.addEventListener("input", () => {
-    localStorage.setItem("customerName", customerNameInput.value);
-});
-customerNameInput.value = localStorage.getItem("customerName");
-
-const customerPhoneNumberInput = document.getElementById("customerPhoneNumber");
-
-const customerAddressInput = document.getElementById("customerAddress");
-customerAddressInput.addEventListener("input", () => {
-    localStorage.setItem("customerAddress", customerAddressInput.value)
-});
-customerAddressInput.value = localStorage.getItem("customerAddress");
-
-const customersPhoneNumberDiv = document.querySelector(".customers-phone-number");
-
-const radioDelivery = document.getElementById("delivery");
-
-customerPhoneNumberInput.addEventListener("input", async () => {
-    const value = customerPhoneNumberInput.value;
-    localStorage.setItem("customerPhoneNumber", value);
-
-    if (value.length < 3) {
-        customersPhoneNumberDiv.hidden = true;
-        customersPhoneNumberDiv.innerHTML = "";
-        return;
-    }
-
-    const response = await fetch(`/Shared/SearchCustomers?phoneNumber=${value}`);
-    const customers = await response.json();
-
-    customersPhoneNumberDiv.innerHTML = "";
-    customersPhoneNumberDiv.hidden = customers.length === 0;
-
-    customers.forEach((customer) => {
-        const div = document.createElement("div");
-        div.style.border = "1px solid #000";
-        div.style.cursor = "pointer";
-        div.textContent = customer.phoneNumber;
-
-        div.addEventListener("click", () => {
-            customerNameInput.value = customer.name;
-            customerPhoneNumberInput.value = customer.phoneNumber;
-            customerAddressInput.value = customer.address;
-            customersPhoneNumberDiv.innerHTML = "";
-            customersPhoneNumberDiv.hidden = true;
-        });
-
-        customersPhoneNumberDiv.appendChild(div);
-    });
-});
-
-customerPhoneNumberInput.value = localStorage.getItem("customerPhoneNumber");
-
-const additionalChargeInput = document.getElementById("additionalCharge");
+﻿const additionalChargeInput = document.getElementById("additionalCharge");
 const gstElement = document.getElementById("gst");
 const pstElement = document.getElementById("pst");
 const subTotalPriceElement = document.getElementById("subTotalPrice");
@@ -66,6 +6,8 @@ const subTotalPrice = parseFloat(subTotalPriceElement.dataset.value) || 0;
 const totalPriceElement = document.getElementById("totalPrice");
 const deliveryFeeInput = document.getElementById("deliveryFee");
 const discountInput = document.getElementById("discount");
+
+const radioDelivery = document.getElementById("delivery");
 
 async function getRoundedValue(num, decimals = 2) {
     const response = await fetch(`/Shared/RoundNumber?value=${num}&decimals=${decimals}`)
@@ -109,12 +51,3 @@ async function updateTotalPrice() {
 additionalChargeInput.addEventListener("input", updateTotalPrice);
 deliveryFeeInput.addEventListener("input", updateTotalPrice);
 discountInput.addEventListener("input", updateTotalPrice);
-
-const orderFormCart = document.getElementById("orderFormCart");
-
-orderFormCart.addEventListener("submit", () => {
-    localStorage.removeItem("observations");
-    localStorage.removeItem("customerName");
-    localStorage.removeItem("customerPhoneNumber");
-    localStorage.removeItem("customerAddress");
-})
---
+++ wwwroot/js/orderFormDetails.js
@@ -0,0 +1,65 @@
+﻿const observationsInput = document.getElementById("observations");
+observationsInput.addEventListener("input", () => {
+    localStorage.setItem("observations", observationsInput.value);
+});
+observationsInput.value = localStorage.getItem("observations");
+
+const customerNameInput = document.getElementById("customerName");
+customerNameInput.addEventListener("input", () => {
+    localStorage.setItem("customerName", customerNameInput.value);
+});
+customerNameInput.value = localStorage.getItem("customerName");
+
+const customersPhoneNumberDiv = document.querySelector(".customers-phone-number");
+
+const customerPhoneNumberInput = document.getElementById("customerPhoneNumber");
+
+customerPhoneNumberInput.addEventListener("input", async () => {
+    const value = customerPhoneNumberInput.value;
+    localStorage.setItem("customerPhoneNumber", value);
+
+    if (value.length < 3) {
+        customersPhoneNumberDiv.hidden = true;
+        customersPhoneNumberDiv.innerHTML = "";
+        return;
+    }
+
+    const response = await fetch(`/Shared/SearchCustomers?phoneNumber=${value}`);
+    const customers = await response.json();
+
+    customersPhoneNumberDiv.innerHTML = "";
+    customersPhoneNumberDiv.hidden = customers.length === 0;
+
+    customers.forEach((customer) => {
+        const div = document.createElement("div");
+        div.style.border = "1px solid #000";
+        div.style.cursor = "pointer";
+        div.textContent = customer.phoneNumber;
+
+        div.addEventListener("click", () => {
+            customerNameInput.value = customer.name;
+            customerPhoneNumberInput.value = customer.phoneNumber;
+            customerAddressInput.value = customer.address;
+            customersPhoneNumberDiv.innerHTML = "";
+            customersPhoneNumberDiv.hidden = true;
+        });
+
+        customersPhoneNumberDiv.appendChild(div);
+    });
+});
+customerPhoneNumberInput.value = localStorage.getItem("customerPhoneNumber");
+
+const customerAddressInput = document.getElementById("customerAddress");
+customerAddressInput.addEventListener("input", () => {
+    localStorage.setItem("customerAddress", customerAddressInput.value)
+});
+customerAddressInput.value = localStorage.getItem("customerAddress");
+
+const orderFormCart = document.getElementById("orderFormCart");
+
+orderFormCart.addEventListener("submit", () => {
+    localStorage.removeItem("observations");
+    localStorage.removeItem("customerName");
+    localStorage.removeItem("customerPhoneNumber");
+    localStorage.removeItem("customerAddress");
+})
