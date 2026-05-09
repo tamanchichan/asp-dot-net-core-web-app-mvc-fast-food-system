e10f35e42adf6755ce44683af24e63d9533fdc66
tamanchichan <tamanchichan@gmail.com>
2026-05-09 11:05:55 -0500

    add localStorage for order's details

--- wwwroot/js/orderFormDetails.js
+++ wwwroot/js/orderFormDetails.js
@@ -1,19 +1,41 @@
-﻿const observationsInput = document.getElementById("observations");
+﻿// Order Type
+const orderTypeRadios = document.querySelectorAll('input[name="orderType"]');
+
+const savedOrderType = localStorage.getItem("orderType");
+
+if (savedOrderType) {
+    const radioToCheck = document.querySelector(
+        `input[name="orderType"][value="${savedOrderType}"]`
+    );
+
+    if (radioToCheck) {
+        radioToCheck.checked = true;
+    }
+};
+
+orderTypeRadios.forEach(radio => {
+    radio.addEventListener("change", function() {
+        localStorage.setItem("orderType", this.value);
+    });
+});
+
+// Observation
+const observationsInput = document.getElementById("observations");
 observationsInput.addEventListener("input", () => {
     localStorage.setItem("observations", observationsInput.value);
 });
 observationsInput.value = localStorage.getItem("observations");
 
+// Customer Name
 const customerNameInput = document.getElementById("customerName");
 customerNameInput.addEventListener("input", () => {
     localStorage.setItem("customerName", customerNameInput.value);
 });
 customerNameInput.value = localStorage.getItem("customerName");
 
+// Customer Phone Number
 const customersPhoneNumberDiv = document.querySelector(".customers-phone-number");
-
 const customerPhoneNumberInput = document.getElementById("customerPhoneNumber");
-
 customerPhoneNumberInput.addEventListener("input", async () => {
     const value = customerPhoneNumberInput.value;
     localStorage.setItem("customerPhoneNumber", value);
@@ -49,17 +71,73 @@ customerPhoneNumberInput.addEventListener("input", async () => {
 });
 customerPhoneNumberInput.value = localStorage.getItem("customerPhoneNumber");
 
+// Customer Address
 const customerAddressInput = document.getElementById("customerAddress");
 customerAddressInput.addEventListener("input", () => {
     localStorage.setItem("customerAddress", customerAddressInput.value)
 });
 customerAddressInput.value = localStorage.getItem("customerAddress");
 
+// Additional Charge
+//const additionalChargeInput = document.getElementById("additionalCharge"); // already define in _OrderForm.js
+additionalChargeInput.addEventListener("input", function () {
+    localStorage.setItem("additionalCharge", this.value);
+});
+additionalChargeInput.value = localStorage.getItem("additionalCharge");
+
+// Delivery Fee
+deliveryFeeInput.addEventListener("input", function () {
+    localStorage.setItem("deliveryFee", this.value);
+});
+deliveryFeeInput.value = localStorage.getItem("deliveryFee");
+
+if (deliveryFeeInput.value !== "" && deliveryFeeInput.value !== "0") {
+    const deliveryRadio = document.getElementById("delivery");
+    deliveryRadio.checked = true;
+}
+
+// Discount
+discountInput.addEventListener("input", function () {
+    localStorage.setItem("discount", this.value);
+});
+discountInput.value = localStorage.getItem("discount");
+
+// Ready Date
+const readyDateOnlyInput = document.getElementById("readyDateOnly");
+readyDateOnlyInput.addEventListener("input", function () {
+    localStorage.setItem("readyDateOnly", this.value);
+})
+
+const readyDateOnly = localStorage.getItem("readyDateOnly");
+
+if (readyDateOnly) {
+    readyDateOnlyInput.value = localStorage.getItem("readyDateOnly");
+}
+
+// Ready Time
+const readyTimeOnlyInput = document.getElementById("readyTimeOnly");
+readyTimeOnlyInput.addEventListener("input", function () {
+    localStorage.setItem("readyTimeOnly", this.value);
+})
+
+const readyTimeOnly = localStorage.getItem("readyTimeOnly");
+
+if (readyTimeOnly) {
+    readyTimeOnlyInput.value = localStorage.getItem("readyTimeOnly");
+}
+
 const orderFormCart = document.getElementById("orderFormCart");
 
 orderFormCart.addEventListener("submit", () => {
+    localStorage.removeItem("orderType");
     localStorage.removeItem("observations");
     localStorage.removeItem("customerName");
     localStorage.removeItem("customerPhoneNumber");
     localStorage.removeItem("customerAddress");
+    localStorage.removeItem("additionalCharge");
+    localStorage.removeItem("deliveryFee");
+    localStorage.removeItem("discount");
+    localStorage.removeItem("readyDateOnly");
+    localStorage.removeItem("readyTimeOnly");
+    //localStorage.clear();
 })
