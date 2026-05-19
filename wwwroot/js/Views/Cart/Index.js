45ff712d9a5051df96e3c0c135399848b9ab9165
tamanchichan <tamanchichan@gmail.com>
2026-05-19 14:08:53 -0500

    Add clearCartProduct JavaScript function

---
+++ wwwroot/js/Views/Cart/Index.js
@@ -0,0 +1,19 @@
+﻿export function clearCartProducts() {
+    fetch("/Cart/ClearCartProducts", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+        .then(response => {
+            if (response.redirected) {
+                localStorage.clear();
+                window.location.href = response.url;
+            } else {
+                window.location.reload();
+            }
+        })
+        .catch(error => {
+            console.error("Error clearing cart:", error);
+        });
+}
